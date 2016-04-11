bb0d96413b38d6dcbb22cf89539adfe94223c66b
APineault <antoine.pineault95@gmail.com>
2016-04-11 12:18:20 -0400

    dasdas
    test1

---
+++ ASPProjectTest/ASPProjectTest/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASPProjectTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Allo patrick";
+
+            return View();
+        }
+    }
+}
---
+++ ASPProjectTest/ASPProjectTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASPProjectTest.MvcApplication" Language="C#" %>
